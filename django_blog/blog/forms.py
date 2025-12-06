d14c40016540883a8e0e8668ac3258bfe06ad77a
Kamohelo <kamohelotshabalala@proton.me>
2025-12-06 23:46:08 +0200

    update blog/forms.py with TagWidget

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -4,105 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
 
-# Custom User Creation Form
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name in self.fields:
-            self.fields[field_name].widget.attrs.update({'class': 'form-control'})
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("This email is already registered.")
-        return email
-
-# Profile Update Form
-class ProfileUpdateForm(forms.ModelForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'first_name', 'last_name']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name in self.fields:
-            self.fields[field_name].widget.attrs.update({'class': 'form-control'})
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        if User.objects.filter(email=email).exclude(pk=self.instance.pk).exists():
-            raise forms.ValidationError("This email is already in use.")
-        return email
-
-# Post Form
-class PostForm(forms.ModelForm):
-    tags = TagField(
-        required=False,
-        widget=TagWidget(attrs={
-            'class': 'form-control',
-            'placeholder': 'Add tags separated by commas'
-        })
-    )
-
-    class Meta:
-        model = Post
-        fields = ['title', 'content', 'tags']
-        widgets = {
-            'title': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter post title'
-            }),
-            'content': forms.Textarea(attrs={
-                'class': 'form-control',
-                'placeholder': 'Write your post content here...',
-                'rows': 10
-            })
-        }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # For editing existing posts, show current tags
-        if self.instance.pk:
-            self.fields['tags'].initial = ', '.join(self.instance.tags.names())
-
-# Comment Form
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['content']
-        widgets = {
-            'content': forms.Textarea(attrs={
-                'class': 'form-control',
-                'placeholder': 'Write your comment here...',
-                'rows': 4
-            })
-        }
-        labels = {
-            'content': ''
-        }
-
-# blog/forms.py
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from .models import Post, Comment
-
-# Import TagWidget from taggit if available
-try:
-    from taggit.forms import TagWidget, TagField
-    TAG_IT_AVAILABLE = True
-except ImportError:
-    TAG_IT_AVAILABLE = False
-    TagWidget = forms.TextInput
-    TagField = forms.CharField
+# Import TagWidget from django-taggit
+from taggit.forms import TagWidget, TagField
 
 # Custom User Creation Form
 class CustomUserCreationForm(UserCreationForm):
@@ -144,25 +47,14 @@ class ProfileUpdateForm(forms.ModelForm):
 
 # Post Form with TagWidget
 class PostForm(forms.ModelForm):
-    # Use TagField and TagWidget if taggit is available
-    if TAG_IT_AVAILABLE:
-        tags = TagField(
-            required=False,
-            widget=TagWidget(attrs={
-                'class': 'form-control',
-                'placeholder': 'Add tags separated by commas'
-            }),
-            help_text="Enter tags separated by commas"
-        )
-    else:
-        tags = forms.CharField(
-            required=False,
-            widget=forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Add tags separated by commas'
-            }),
-            help_text="Enter tags separated by commas"
-        )
+    tags = TagField(
+        required=False,
+        widget=TagWidget(attrs={
+            'class': 'form-control',
+            'placeholder': 'Add tags separated by commas'
+        }),
+        help_text="Enter tags separated by commas"
+    )
 
     class Meta:
         model = Post
@@ -184,19 +76,7 @@ class PostForm(forms.ModelForm):
 
         # Initialize tags field with current tags for editing
         if self.instance.pk and hasattr(self.instance, 'tags'):
-            if TAG_IT_AVAILABLE:
-                self.fields['tags'].initial = ', '.join(self.instance.tags.names())
-
-    def clean_tags(self):
-        if TAG_IT_AVAILABLE:
-            return self.cleaned_data.get('tags', '')
-        else:
-            tags_input = self.cleaned_data.get('tags', '')
-            # Basic validation for custom tag implementation
-            tags = [tag.strip() for tag in tags_input.split(',') if tag.strip()]
-            if len(tags) > 10:
-                raise forms.ValidationError("You can add up to 10 tags.")
-            return tags_input
+            self.fields['tags'].initial = ', '.join(self.instance.tags.names())
 
 # Comment Form
 class CommentForm(forms.ModelForm):
